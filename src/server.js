e11b1f4b75c22a861541a52669bfb674aa751c6c
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-02 20:02:52 -0300

    build: cors options issue

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const server = express();
 server.use(cors());
+server.options('*', cors());
 server.use(express.json());
 server.use(router);
 server.get("/health", (req, res) => {
