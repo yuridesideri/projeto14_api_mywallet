ef452bb8cab89f616606451b276011dd8493901c
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-02 14:46:44 -0300

    feat: health check

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import router from "./routes/index.routers.js";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(router);
-
+server.get("/health", (req, res) => {
+	res.send("OK");
+});
 
 const port = process.env.SERVER_PORT || 5000;
 server.listen(port, () => console.log(`Starting in port ${port}`));
